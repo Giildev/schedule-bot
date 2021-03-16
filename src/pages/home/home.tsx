a3d29b5135a6674c73a8040c030a595a76554c12
Gil <gilberto.marcano@somoglobal.com>
2021-03-15 19:29:11 -0500

    custom hook used on home to test

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,10 +3,17 @@ import Row from '@paljs/ui/Row';
 import Col from '@paljs/ui/Col';
 import Container from '@material-ui/core/Container';
 import Table from '../../components/Table/Table';
+import useGetSchedule from '../../firebase/hooks/useGetSchedule';
 
 import SEO from '../../components/SEO';
 
 const Home = () => {
+  const [data, loading, error] = useGetSchedule();
+
+  console.log(loading);
+  console.log(data);
+  console.log(error);
+
   return (
     <>
       <SEO title="Basic Home" />
